401938e9a5c7fba93026a237e818b7eeddfc985f
Henrique Naimaier <coubron@yahoo.com.br>
2021-05-19 10:09:48 -0300

    First commit

---
+++ api/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const config = require('config')
+
+app.use(bodyParser.json())
+app.listen(config.get(api.porta), () => console.log('API rodando!'))
